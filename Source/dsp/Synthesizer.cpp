b5dbe672258d60f0587b77ecb8773ce7f9a2e5d6
Corredor1230 <juanpablocorredor97@gmail.com>
2026-06-01 13:42:25 +0900

    New synthesizer class and structure

---
+++ Source/dsp/Synthesizer.cpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "Synthesizer.h"
+
+void SynthVoice::renderNextBlock (juce::AudioBuffer<float>& outputBuffer, 
+int startSample, int numSamples) override
+{
+    
+}
---
+++ Source/dsp/Synthesizer.h
@@ -0,0 +1,54 @@
+#pragma once
+#include<JuceHeader.h>
+
+class SynthSound : public juce::SynthesiserSound
+{
+public:
+    bool appliesToNote (int /*midiNoteNumber*/) override  { return true; }
+    bool appliesToChannel (int /*midiChannel*/) override { return true; }
+};
+
+// The actual rendering engine for a single voice
+class SynthVoice : public juce::SynthesiserVoice
+{
+public:
+    bool canPlaySound (juce::SynthesiserSound* sound) override
+    {
+        return dynamic_cast<SynthSound*> (sound) != nullptr;
+    }
+
+    void startNote (int midiNoteNumber, float velocity, 
+                    juce::SynthesiserSound* sound, int currentPitchWheelPosition) override
+    {
+        isTargetNoteActive = true;
+        level = velocity;
+        
+        // Convert MIDI note to frequency
+        auto cyclesPerSecond = juce::MidiMessage::getMidiNoteInHertz(midiNoteNumber);
+    }
+
+    void stopNote (float velocity, bool allowTailOff) override
+    {
+        if (allowTailOff)
+        {
+            // Start release phase
+            isTargetNoteActive = false; 
+        }
+        else
+        {
+            // Shut off instantly
+            clearCurrentNote();
+        }
+    }
+
+    void renderNextBlock (juce::AudioBuffer<float>& outputBuffer, 
+                          int startSample, int numSamples) override
+
+    void pitchWheelMoved (int newPitchWheelValue) override {}
+    void controllerMoved (int controllerNumber, int newControllerValue) override {}
+
+private:
+    float level = 0.0f;
+    float phase = 0.0f;
+    bool isTargetNoteActive = false;
+};
